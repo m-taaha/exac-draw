c98319d497f11a09ea5f4c89686f0e328006da93
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-28 22:37:45 +0530

    feat(ws): fix environment variable import

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,43 +1,40 @@
-import { configDotenv } from "dotenv";
+import "dotenv/config";
 import { WebSocketServer, WebSocket } from "ws";
-import {IncomingMessage} from "http";
+import { IncomingMessage } from "http";
 import cookie from "cookie";
 import jwt from "jsonwebtoken";
 
-configDotenv();
 
-const wss = new WebSocketServer({port: 8080});
+
+const wss = new WebSocketServer({ port: 8080 });
 
 const JWT_SECRET = process.env.JWT_SECRET_KEY;
-if(!JWT_SECRET) {
-    throw new Error("JWT_SECRET_KEY not defined in environment variable")
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET_KEY not defined in environment variable");
 }
 
-
 wss.on("connection", (ws: WebSocket, req: IncomingMessage) => {
-    try {
-            const rawCookie = req.headers.cookie;
-            if (!rawCookie) return ws.close();
+  try {
+    const rawCookie = req.headers.cookie;
+    if (!rawCookie) return ws.close();
 
-            const parsed = cookie.parse(rawCookie);
-            const token = parsed.token;
-            if (!token) return ws.close();
+    const parsed = cookie.parse(rawCookie);
+    const token = parsed.token;
+    if (!token) return ws.close();
 
-            const decode = jwt.verify(token, JWT_SECRET) as { userId: string };
-            const userId = decode.userId;
+    const decode = jwt.verify(token, JWT_SECRET) as { userId: string };
+    const userId = decode.userId;
 
-            console.log("User connected:", userId);
+    console.log("User connected:", userId);
 
-            ws.on("message", (data) => {
-                console.log("Message from", userId, ":", data.toString());
+    ws.on("message", (data) => {
+      console.log("Message from", userId, ":", data.toString());
+    });
 
-            });
-
-
-            ws.on("close", () => {
-                console.log("User disconnected:", userId)
-            })
-    } catch (error) {
-        ws.close();
-    }
-})
+    ws.on("close", () => {
+      console.log("User disconnected:", userId);
+    });
+  } catch (error) {
+    ws.close();
+  }
+});
