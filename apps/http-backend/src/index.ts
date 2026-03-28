83411796510a9d45a9e9da327c1c894fc355475e
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-28 14:18:28 +0530

    chore(server): configure CORS to allow frontend origin and enable cookies for auth

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import userRouter from "./routes/userRoutes.js";
 import roomRouter from "./routes/roomRoutes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app = express();
 
@@ -10,6 +11,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true
+}))
 
 
 app.use("/api/v1/user", userRouter);
