4a3ff7ac885b58bdd593231de1d659747956d237
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-28 15:45:48 +0530

    feat(ws-backend): implement websocket authentication using cookies and JWT

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,40 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
+import {IncomingMessage} from "http";
+import cookie from "cookie";
+import jwt from "jsonwebtoken";
 
 const wss = new WebSocketServer({port: 8080});
 
-wss.on("connection", function connection(ws) {
-    ws.on("message", function message(data) {
-        ws.send("pong")
-    })
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+if(!JWT_SECRET) {
+    throw new Error("JWT_SECRET_KEY not defined in environment variable")
+}
+
+
+wss.on("connection", (ws: WebSocket, req: IncomingMessage) => {
+    try {
+            const rawCookie = req.headers.cookie;
+            if (!rawCookie) return ws.close();
+
+            const parsed = cookie.parse(rawCookie);
+            const token = parsed.token;
+            if (!token) return ws.close();
+
+            const decode = jwt.verify(token, JWT_SECRET) as { userId: string };
+            const userId = decode.userId;
+
+            console.log("User connected:", userId);
+
+            ws.on("message", (data) => {
+                console.log("Message from", userId, ":", data.toString());
+
+            });
+
+
+            ws.on("close", () => {
+                console.log("User disconnected:", userId)
+            })
+    } catch (error) {
+        ws.close();
+    }
 })
