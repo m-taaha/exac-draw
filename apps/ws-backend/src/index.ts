97ec3d62550d087398ccf3cf2afe5decbd2a0627
Mohammad Taaha Ashraf <145143787+m-taaha@users.noreply.github.com>
2026-03-29 11:57:43 +0530

    feat(ws): add webSocket server with room management and cookie auth

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -3,6 +3,7 @@ import { WebSocketServer, WebSocket } from "ws";
 import { IncomingMessage } from "http";
 import cookie from "cookie";
 import jwt from "jsonwebtoken";
+import { parse } from "url";
 
 
 
@@ -12,6 +13,9 @@ const JWT_SECRET = process.env.JWT_SECRET_KEY;
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET_KEY not defined in environment variable");
 }
+
+
+    const rooms = new Map<string, Set<WebSocket>>(); //this creaets a data structure to store rooms and users inside them 
 
 wss.on("connection", (ws: WebSocket, req: IncomingMessage) => {
   try {
@@ -25,13 +29,35 @@ wss.on("connection", (ws: WebSocket, req: IncomingMessage) => {
     const decode = jwt.verify(token, JWT_SECRET) as { userId: string };
     const userId = decode.userId;
 
+
+    // parse the url and extract the query from it. then extract room id from the query
+    const {query} = parse(req.url!, true);
+    const roomId = query.roomId as string;
+    if(!roomId) return ws.close();
+    
+
+    // this will check if the room has aleady this room id? if not then create it 
+    if (!rooms.has(roomId)) {
+      rooms.set(roomId, new Set());
+    }
+
+    rooms.get(roomId)!.add(ws); //add the user to the room
+
+
+
     console.log("User connected:", userId);
 
     ws.on("message", (data) => {
-      console.log("Message from", userId, ":", data.toString());
+      rooms.get(roomId)?.forEach(client => {
+        // send the message to everyone in room but not the user itself because he created the message or generated
+        if(client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data.toString());
+        }
+      });
     });
 
     ws.on("close", () => {
+      rooms.get(roomId)?.delete(ws);
       console.log("User disconnected:", userId);
     });
   } catch (error) {
